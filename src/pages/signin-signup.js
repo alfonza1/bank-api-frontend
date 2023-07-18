1e22a56ffe3a2d80ab640b81bf66137bfa23db45
Angel Borja <borjaangel244@gmail.com>
2023-07-18 16:49:50 -0400

    added routes

---
+++ src/pages/signin-signup.js
@@ -0,0 +1,33 @@
+import React, { useState } from "react";
+import CreateAnAccount from "./createaccount";
+import { Link } from 'react-router-dom';
+
+
+const SigninSignUp = () => {
+
+    const [showCreateAccount, setShowCreateAccount] = useState(false);
+
+    const handleSignUpClick = () => {
+        setShowCreateAccount(true);
+    };
+
+
+
+    return (
+        <div>
+            <h1>Welcome to Gradient Bank&trade;</h1>
+            <h2>Get started with the #1 bank in the world today!</h2>
+            <Link to="/createaccount"> {/* Use Link to navigate to "/createaccount" */}
+        <button type="button" className="btn btn-secondary">Sign up</button>
+      </Link>
+            {/* <a href="/createaccount.js">
+            <button type="button" className="btn btn-secondary">Sign in</button>
+            </a> <a href="/createaccount.js">
+            <button type="button" className="btn btn-secondary">Sign up</button>
+            </a> */}
+            {showCreateAccount && <CreateAnAccount />}
+        </div>
+    );
+};
+
+export default SigninSignUp;
