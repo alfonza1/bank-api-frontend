e0d17c4438bdb2b3e041a88959303b0720e5688c
Angel Borja <borjaangel244@gmail.com>
2023-07-18 14:15:43 -0400

    added components and service

---
+++ src/services/AccountInfoService.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+const Accounts = ({ type, nickname, rewards, balance }) => {
+
+
+
+
+
+    return (
+        <div className="accounts">
+            <h1>Accounts</h1>
+        </div>
+
+
+
+
+
+
+
+    );
+};
+
+export default Accounts;
---
+++ src/services/TransactionService.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+const Transaction = ({ deposits, withdrawals, transfers }) => {
+
+
+
+
+
+    return (
+        <div className="transaction">
+            <h1>Transaction</h1>
+        </div>
+
+
+
+
+
+
+
+    );
+};
+
+export default Transaction;
